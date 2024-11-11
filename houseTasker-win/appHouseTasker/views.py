a3b9c8687d42e6c86d134b9b3166a245932fc1c7
julenpe12 <87939099+julenpe12@users.noreply.github.com>
2024-11-11 16:34:43 +0100

    Task Detail Functionality
    added task and resource to admin.py
    created task_detail.html
    changed task_list.html to access details from name
    created task_detail functionality

--- houseTasker-win/appHouseTasker/views.py
+++ houseTasker-win/appHouseTasker/views.py
@@ -10,6 +10,7 @@ from .models import Resource
 from .models import Task
 from .forms import TaskForm
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 @login_required
 def home(request):
@@ -34,7 +35,8 @@ def task_create(request):
 
 @login_required
 def task_detail(request, task_id):
-    return HttpResponse(f"Viewing task {task_id}")
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'task_detail.html', {'task': task})
 
 @login_required
 def task_edit(request, task_id):
@@ -42,7 +44,11 @@ def task_edit(request, task_id):
 
 @login_required
 def task_delete(request, task_id):
-    return HttpResponse(f"Delete task {task_id}")
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'task_delete.html', {'task': task})
 
 @login_required
 def resource_list(request):
